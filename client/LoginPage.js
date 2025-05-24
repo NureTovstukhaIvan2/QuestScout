33d9230c1c6e3377b660246ab9b4f62ae5318961
Ivan Tovstukha <ivan.tovstukha@nure.ua>
2025-05-24 22:58:19 +0300

    login

---
+++ client/LoginPage.js
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div className="bg-zinc-950 min-h-screen flex justify-center font-roboto text-slate-100">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default ScrollToTop(LoginPage);
